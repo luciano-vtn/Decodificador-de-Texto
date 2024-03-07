153701d45ae71c089b37be79f75e633c1d62869f
Luciano Sousa - vtn <luciano.vtn120@gmail.com>
2024-03-07 17:11:41 -0300

    btnCopiar finalizado

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
 
- const textDaArea = document.querySelector(".container__input");
- const resultadoText = document.querySelector(".container__input_resultado");
+const textDaArea = document.querySelector(".container__input");
+const resultadoText = document.querySelector(".container__input_resultado");
+const copiarTexto = document.querySelector(".botao__copiar");
+
+
+function criptografar(string) {
+    return string
+        .replaceAll("a", "ai")
+        .replaceAll("e", "enter")
+        .replaceAll("o", "ober")
+        .replaceAll("u", "ufat");
+};
+
+function descriptografar(string) {
+    return string
+        .replaceAll("ai", "a")
+        .replaceAll("enter", "e")
+        .replaceAll("imes", "i")
+        .replaceAll("ober", "o")
+        .replaceAll("ufat", "u");
+        
+};
 
 function btnCriptografar(){
-
     const textoEncriptado = criptografar(textDaArea.value);
     resultadoText.value = textoEncriptado;
     textDaArea.value = "";
-};
+}
 
 function btnDescriptografar(){
     const textoDesencriptado = descriptografar(textDaArea.value);
     resultadoText.value = textoDesencriptado;
     textDaArea.value = "";
+}
+
+function btnCopiar() {
+    textDaArea.value = resultadoText.value;
+    resultadoText.value = "";
 };
-
-function criptografar(string) {
-    const resultadoText = string
-    .replaceAll("a", "ai")
-    .replaceAll("e", "enter")
-    .replaceAll("i", "imes")
-    .replaceAll("o", "ober")
-    .replaceAll("u", "ufat");
-    return resultadoText;
-}
-
-function descriptografar(string) {
-    const resultadoText = string
-    .replaceAll("ai", "a")
-    .replaceAll("enter", "e")
-    .replaceAll("ober", "o")
-    .replaceAll("ufat", "u")
-    .replaceAll("imes", "i");
-    return resultadoText;
-}
-        /* falta fazer o btnCopir funcionar */
-
-
-function btnCopiar( resultadoText){
-    return textDaArea;
-}
